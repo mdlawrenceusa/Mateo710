d1e69d676d7c40fbf15247d7db1bdcf6d673cb62
mdlawrenceusa <mdlawrence@me.com>
2020-12-16 13:11:44 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-zeksbe-mtd2oe
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-zeksbe-mtd2oe)
---
+++ src/app/mateo/mateo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mateo',
+  templateUrl: './mateo.component.html',
+  styleUrls: ['./mateo.component.css']
+})
+export class MateoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
